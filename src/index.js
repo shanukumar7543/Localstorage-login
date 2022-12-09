5f82839ffa7288494a43cb0ef4cbb92a42e1d205
shanukumar7543 <shanukumar7543@gmail.com>
2022-12-09 08:18:23 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import TextSlider from './Components/TextSlider/TextSlider';
+import HtmlEditor from './Components/htmlEditor/Htmleditor';
+import GetApiCall from './Components/FetchApiCall/GetApiCall';
+import DynamicInput from './Components/Loading/Loading';
+import Form from './Components/FormValidation/Form';
+import RoutingReact from './Components/Routing/Routing';
+
+ReactDOM.render(
+    // <RoutingReact />,
+    <App/>,
+  document.getElementById('root')
+);
